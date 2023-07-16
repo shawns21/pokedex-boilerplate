cd6a573083e77ee5f441176560ba603474c8e81e
shawns21 <shawnsinanan21@gmail.com>
2023-07-15 20:40:23 -0400

    finished some of the project

--- app.js
+++ app.js
@@ -1,10 +1,52 @@
 const express = require("express");
-
 const app = express();
+
+const morgan = require("morgan");
+
+app.use(morgan("dev"));
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => res.send("Hello World!"));
+const pokeBank = require('./pokeBank');
+
+const pokemon = pokeBank.list();
+
+app.get("/", (req, res) => res.send(
+  `<!DOCTYPE html>
+  <html>
+    <head>
+      <title>My Pokedex</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+      <div class="pokemon-list">
+        <header><img src="/logo.png" />Pokedex</header>
+        ${pokemon
+          .map(
+            (pokemon) => `
+        <div class="pokemon-item">
+          <img class="pokemon-img" src={pokemon.image} />
+          <p>
+            <span class="pokemon-position">${pokemon.id}. ▲</span>${pokemon.name}
+            <small>(Trained by ${pokemon.trainer})</small>
+          </p>
+          <small class="pokemon-info">
+            Type: ${pokemon.type} | Date Caught: ${pokemon.date}
+          </small>
+        </div>
+        `
+          )
+          .join("")}
+      </div>
+    </body>
+  </html>
+  `));
+
+app.get("/pokemon/:id", (req, res) => {
+  const id = req.params.id;
+  const post = pokeBank.find(id);
+  res.send("test");
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
